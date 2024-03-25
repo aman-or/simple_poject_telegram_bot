8c65f29066f007e978ffdd04d21901e627dc75a4
aman-or <a-o-b001@mail.ru>
2024-03-25 14:15:26 +0300

    simple program weather

---
+++ weather/main.py
@@ -0,0 +1,31 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot('6864089062:AAHfAt8eoqZYhV5q75eXdmOOM0Ua1Hk16-s')
+API = '1dab56cc2eb7d368875015a88372deb5'
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hi nice to see you, please write your country name:')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        wind = data["wind"]["speed"]
+        bot.reply_to(message, f'Weather now: {temp}° C\n Wind now: {wind}')
+
+        image = 'rain.png' if temp < 5.0 else 'sunAndCloud.png'
+        file = open('weather/' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, f'Weather wrote wrong')
+
+
+bot.polling(non_stop=True)
