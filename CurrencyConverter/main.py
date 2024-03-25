8ef3f3123e44a4f0110101a537bf7c556a6b1a3d
aman-or <a-o-b001@mail.ru>
2024-03-25 19:30:47 +0300

    simple currency converter

---
+++ CurrencyConverter/main.py
@@ -0,0 +1,60 @@
+import telebot
+from currency_converter import CurrencyConverter
+from telebot import types
+
+bot = telebot.TeleBot('6864089062:AAHfAt8eoqZYhV5q75eXdmOOM0Ua1Hk16-s')
+currency = CurrencyConverter()
+amount = 0
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hi! input currency')
+    bot.register_next_step_handler(message, summa)
+
+
+def summa(message):
+    global amount
+    try:
+        amount = int(message.text.strip())
+    except ValueError:
+        bot.send_message(message.chat.id, 'Not true format. Input digit')
+        bot.register_next_step_handler(message, summa)
+        return
+    if amount > 0:
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        btn1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
+        btn2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
+        btn3 = types.InlineKeyboardButton('RUB/USD', callback_data='rub/usd')
+        btn4 = types.InlineKeyboardButton('else value', callback_data='else')
+        markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.chat.id, 'Выберите пару валют', reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, 'Digit should be longer than 0 . Input digit')
+        bot.register_next_step_handler(message, summa)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data != 'else':
+        values = call.data.upper().split('/')
+        res = currency.convert(amount, values[0], values[1])
+        bot.send_message(call.message.chat.id, f'Convert: {round(res, 2)}. You can input your currency again:')
+        bot.register_next_step_handler(call.message, summa)
+    else:
+        bot.send_message(call.message.chat.id, 'input your value with .../...')
+        bot.register_next_step_handler(call.message, my_currency)
+
+
+def my_currency(message):
+    try:
+        values = message.text.upper().split('/')
+        res = currency.convert(amount, values[0], values[1])
+        bot.send_message(message.chat.id, f'Convert: {round(res, 2)}. You can input your currency again:')
+        bot.register_next_step_handler(message, summa)
+    except Exception:
+        bot.send_message(message.chat.id, 'Something get wrong, please try again, input value:')
+        bot.register_next_step_handler(message, summa)
+
+
+bot.polling(non_stop=True)
