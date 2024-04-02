c34b5412f0715e7c73164b411dd4791fa18e7d55
aman-or <a-o-b001@mail.ru>
2024-04-02 22:07:42 +0300

    html for gitpages

--- web soft/app.py
+++ web soft/app.py
@@ -1,3 +1,5 @@
+import json
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 
@@ -8,7 +10,13 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Open web sote', web_app=WebAppInfo(url='https://www.youtube.com/')))
-    await message.answer('Hi, my friend', reply_markup=markup)
+    markup.add(types.KeyboardButton('Open web site', web_app=WebAppInfo(url='https://aman-or.github.io/page/')))
+    await message.answer(f'Hi, {message.from_user.first_name}', reply_markup=markup)
+
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Name: {res["name"]}.\n Email: {res["email"]}.\n Phone: {res["phone"]}')
 
 executor.start_polling(dp)
